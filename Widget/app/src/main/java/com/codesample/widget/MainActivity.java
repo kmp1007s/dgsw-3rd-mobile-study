4e44f65ae63fa33bc94304e1347700d649c31ee1
SaeHan Kim <kmp1007s@naver.com>
2020-04-21 16:14:21 +0900

    200421 ViewBinding
    200421 ViewBinding

--- Widget/app/src/main/java/com/codesample/widget/MainActivity.java
+++ Widget/app/src/main/java/com/codesample/widget/MainActivity.java
@@ -3,12 +3,74 @@ package com.codesample.widget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
 
-public class MainActivity extends AppCompatActivity {
+import com.codesample.widget.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements TextWatcher, View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater()); // inflate는 xml파일을 메모리상에 띄움
+        setContentView(binding.getRoot());
+        initWidgets();
+    }
+
+    private void initWidgets() {
+        binding.editTextName.addTextChangedListener(this);
+        binding.editTextPhone.addTextChangedListener(this);
+        binding.radioButtonAdult.setOnClickListener(this);
+        binding.radioButtonStudent.setOnClickListener(this);
+        binding.checkBoxTerms.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        Log.i("Main", "before=" + s + " start=" + start + ", count=" + count + ", after=" + after);
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        Log.i("Main", "on=" + s + " start=" + start + ", before=" + before + ", count=" + count);
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        Log.i("Main", "after=" + s.toString());
+        updateProgress();
+    }
+
+    @Override
+    public void onClick(View v) {
+        updateProgress();
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        updateProgress();
+    }
+
+    private void updateProgress() {
+        int progress = 0;
+
+        if(binding.editTextName.getText().length() > 0) progress++;
+        if(binding.editTextPhone.getText().length() > 0) progress++;
+        if(binding.radioButtonAdult.isChecked() || binding.radioButtonStudent.isChecked()) progress++;
+        if(binding.checkBoxTerms.isChecked()) progress++;
+
+        binding.progressBar.setProgress(progress);
+        if(progress == 4) binding.buttonApply.setVisibility(View.VISIBLE);
+        else binding.buttonApply.setVisibility(View.INVISIBLE);
     }
 }
